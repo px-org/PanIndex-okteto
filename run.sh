df91376a378129ac92783bce4645ccd84b349875
libsgh <woiyyng@gmail.com>
2022-03-20 17:50:54 +0800

    :sparkles:update

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 version="$PAN_INDEX_VERSION"
 export TZ=Asia/Shanghai
-export DB_TYPE=postgres
-export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRESQL_HOST}:5432/${POSTGRES_DB}"
 if [ "$version" = "" ]
 then
     TAG_URL="https://api.github.com/repos/libsgh/PanIndex/releases"
@@ -12,10 +10,22 @@ then
         -s "${TAG_URL}" --connect-timeout 20 | awk -F'[ "]+' '$0~"tag_name"{print $4;exit}' )"
 fi
 env
-echo ${version}
+cat > "/app/config.json" << EOF
+{
+  "host": "0.0.0.0",
+  "port": 5238,
+  "log_level": "info",
+  "data_path": "/app/data",
+  "cert_file": "",
+  "key_file": "",
+  "config_query": "",
+  "db_type": "postgres",
+  "dsn": "host=${POSTGRESQL_HOST} user=${POSTGRESQL_USERNAME} password=${POSTGRESQL_PASSWORD} dbname=${POSTGRESQL_DATABASE} port=${DB_SERVICE_PORT} sslmode=require TimeZone=Asia/Shanghai"
+}
+EOF
 curl -sOL "https://github.com/libsgh/PanIndex/releases/download/${version}/PanIndex-linux-amd64.tar.gz"
 tar --same-owner -zxf "PanIndex-linux-amd64.tar.gz"
 rm -rf README.md LICENSE
 mv PanIndex-linux-amd64 PanIndex
 chmod +x PanIndex
-/app/PanIndex
+/app/PanIndex -c=/app/config.json
