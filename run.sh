e8a0eb86e72d8fcb4a5b77f663aa92930ecdd58e
libsgh <woiyyng@gmail.com>
2022-03-20 15:07:24 +0800

    :sparkles:update

--- run.sh
+++ run.sh
@@ -7,10 +7,7 @@ then
         | sed -E 's/.*"([^"]+)".*/\1/'`
 fi
 curl -sOL "https://github.com/libsgh/PanIndex/releases/download/${version}/PanIndex-linux-amd64.tar.gz"
-ls -n
-echo $version
-md5sum "PanIndex-linux-amd64.tar.gz"
-tar -zxvf "PanIndex-linux-amd64.tar.gz"
+tar --same-owner -zxf "PanIndex-linux-amd64.tar.gz"
 rm -rf README.md LICENSE
 mv PanIndex-linux-amd64 PanIndex
 chmod +x PanIndex
