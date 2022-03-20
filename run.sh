914d8bb25b345fecd96026a997938a027d23c338
libsgh <woiyyng@gmail.com>
2022-03-20 17:55:38 +0800

    :sparkles:update

--- run.sh
+++ run.sh
@@ -20,9 +20,10 @@ cat > "/app/config.json" << EOF
   "key_file": "",
   "config_query": "",
   "db_type": "postgres",
-  "dsn": "host=${POSTGRESQL_HOST} user=${POSTGRESQL_USERNAME} password=${POSTGRESQL_PASSWORD} dbname=${POSTGRESQL_DATABASE} port=${DB_SERVICE_PORT} sslmode=require TimeZone=Asia/Shanghai"
+  "dsn": "host=${POSTGRESQL_HOST} user=${POSTGRESQL_USERNAME} password=${POSTGRESQL_PASSWORD} dbname=${POSTGRESQL_DATABASE} port=${DB_SERVICE_PORT} TimeZone=Asia/Shanghai"
 }
 EOF
+cat /app/config.json
 curl -sOL "https://github.com/libsgh/PanIndex/releases/download/${version}/PanIndex-linux-amd64.tar.gz"
 tar --same-owner -zxf "PanIndex-linux-amd64.tar.gz"
 rm -rf README.md LICENSE
