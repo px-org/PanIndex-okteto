cb6f7df8cf5cf86c6f8446e62634c5b0eeb686c7
libsgh <woiyyng@gmail.com>
2022-03-21 11:06:39 +0800

    :sparkles:update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PanIndex-okteto
+
+[![Develop on Okteto](https://okteto.com/develop-okteto.svg)](https://cloud.okteto.com/deploy?repository=https://github.com/libsgh/PanIndex-okteto&branch=main)
+
+**如果想锁定版本，请Fork本仓库后修改run.sh中的版本号，否则将会自动部署最新Release版本**
+
+### 相关
+[PanIndex](https://github.com/libsgh/PanIndex)
--- run.sh
+++ run.sh
@@ -9,7 +9,6 @@ then
         -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0" \
         -s "${TAG_URL}" --connect-timeout 20 | awk -F'[ "]+' '$0~"tag_name"{print $4;exit}' )"
 fi
-env
 cat > "/app/config.json" << EOF
 {
   "host": "0.0.0.0",
@@ -23,7 +22,6 @@ cat > "/app/config.json" << EOF
   "dsn": "host=${POSTGRESQL_HOST} user=${POSTGRESQL_USERNAME} password=${POSTGRESQL_PASSWORD} dbname=${POSTGRESQL_DATABASE} port=${DB_SERVICE_PORT} sslmode=disable"
 }
 EOF
-cat /app/config.json
 curl -sOL "https://github.com/libsgh/PanIndex/releases/download/${version}/PanIndex-linux-amd64.tar.gz"
 tar --same-owner -zxf "PanIndex-linux-amd64.tar.gz"
 rm -rf README.md LICENSE
